da9c40640caae4fb3faf65c99d563c19b408c4d7
BrianLoe <corneliusbrian.loe@students.mq.edu.au>
2023-09-30 17:56:26 +1000

    modify tool input to accept one param
    get_price_change_pct is modified to accept one string parameter to reduce redundancy

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,8 +1,8 @@
 import yfinance as yf
 import os
 import streamlit as st
-import json
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 from typing import Optional, Type
@@ -12,6 +12,7 @@ from langchain.schema import HumanMessage
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
+from langchain import OpenAI
 from langchain.tools import DuckDuckGoSearchRun
 from langchain.callbacks import StreamlitCallbackHandler
 
@@ -39,8 +40,9 @@ def get_history_prices(code, days_ago):
     
     return hist_data, currency
 
-def get_price_change_pct(code, days_ago):    
-    hist_data, _ = get_history_prices(code, days_ago)
+def get_price_change_pct(stockticker_days):
+    code, days_ago = stockticker_days.split(',')    
+    hist_data, _ = get_history_prices(code, int(days_ago))
     
     old_price = hist_data['Close'].iloc[0]
     new_price = hist_data['Close'].iloc[-1]
@@ -52,6 +54,7 @@ def get_price_change_pct(code, days_ago):
 def get_hist_price_plot(code, days_ago):
     hist_data, currency = get_history_prices(code, days_ago)
     hist_data = hist_data['Close']
+    
     fig = px.line(
         x=hist_data.index,
         y=hist_data.values,
@@ -60,7 +63,42 @@ def get_hist_price_plot(code, days_ago):
             'x': 'Date',
             'y': f'Price ({currency})'
         }
-    )
+    ).update_traces(line_color='gray')
+    
+    max_idx, max_val = hist_data.idxmax(), hist_data[hist_data.idxmax()]
+    min_idx, min_val = hist_data.idxmin(), hist_data[hist_data.idxmin()]
+    
+    fig.add_trace(go.Scatter(
+        x=[max_idx],
+        y=[max_val],
+        mode='text+markers',
+        text=['Highest'],
+        textposition="middle left",
+        showlegend=False,
+        marker=dict(
+            color='crimson'
+        ),
+        textfont=dict(
+            size=14,
+            color="crimson"
+        )
+    ))
+    
+    fig.add_trace(go.Scatter(
+        x=[min_idx],
+        y=[min_val],
+        mode='text+markers',
+        text=['Lowest'],
+        textposition="middle left",
+        showlegend=False,
+        marker=dict(
+            color='blue'
+        ),
+        textfont=dict(
+            size=14,
+            color="blue"
+        )
+    ))
     
     return fig
 
@@ -106,7 +144,7 @@ class StockPriceCheckInput(BaseModel):
 
 class StockPriceTool(BaseTool):
     name = "get_stock_ticker_price"
-    description = "Useful for when you need to find out the price of stock. You should input the stock ticker used on the yfinance API"
+    description = "Useful for when you need to find out the price of a stock. You should input the stock ticker used on the yfinance API"
 
     def _run(self, stockticker: str):
         # print("i'm running")
@@ -120,17 +158,16 @@ class StockPriceTool(BaseTool):
     args_schema: Optional[Type[BaseModel]] = StockPriceCheckInput
     
 class StockPctChangeCheckInput(BaseModel):
-    """Input for stock ticker check for percentage check"""
+    """Input for stock ticker for percentage or trend check"""
     
-    stockticker: str = Field(..., description='Ticker symbol for stock or index')
-    days_ago: int = Field(..., description='Int number of days to look back')
+    input_string: str = Field(..., description='a comma separated list with length of 2 consisting of string and number')
     
 class StockPctChangeTool(BaseTool):
     name = 'get_price_change_percent'
-    description = "Useful when you need to find out the percentage change in a stock's value. You should input the stock ticker used on the yfinance API and also input the number of days to check the change over"
+    description = "Useful when you need to find out the percentage change or trend in a stock's price. You should input the stock ticker used on the yfinance API and also input the number of days to check the change over. The input to this tool should be a comma separated list with length of 2 consisting of string representing the stock ticker and number representing days to look back. For example, `MSFT,30` would be the input if you wanted to find out Microsoft stock price over 30 days ago."
     
-    def _run(self, stockticker: str, days_ago: int):
-        price_change_response = get_price_change_pct(stockticker, days_ago)
+    def _run(self, stockticker_days: str):
+        price_change_response = get_price_change_pct(stockticker_days)
         return price_change_response
     
     def _arun(self, stockticker: str, days_ago: int):
@@ -139,21 +176,21 @@ class StockPctChangeTool(BaseTool):
     args_schema: Optional[Type[BaseModel]] = StockPctChangeCheckInput
     
 class StockBestPerformingInput(BaseModel):
-    """Input for Stock ticker check. for percentage check"""
+    """Input for Stock ticker. For performance comparison percentage check"""
 
     stocktickers: list[str] = Field(..., description="Ticker symbols for stocks or indices")
     days_ago: int = Field(..., description="Int number of days to look back")
     
 class StockGetBestPerformingTool(BaseTool):
     name = 'get_best_performing_stock'
-    description = 'Useful for when you need to compare performance of multiple stocks over a period. You should input a list of stock tickers used on the yfinance API and also input the number of days to check the change over'
+    description = 'Useful for when you need to compare performance of multiple stocks trend over a period. You should input a list of stock tickers used on the yfinance API and also input the number of days to check the change over'
     
     def _run(self, stocktickers: list[str], days_ago: int):
         response = get_best_performing_stock(stocktickers, days_ago)
 
         return response
     
-    def _arun(self, stockticker: list[str], days_ago: int):
+    def _arun(self, stocktickers: list[str], days_ago: int):
         raise NotImplementedError("This tool does not support async")
 
     args_schema: Optional[Type[BaseModel]] = StockBestPerformingInput
@@ -168,27 +205,33 @@ if __name__=='__main__':
         func=search.run,
         description='useful for when you need to answer questions about current events. You should ask targeted questions'
     )
-    # Web UI Layout
+    # Streamlit UI Layout
     st.set_page_config(
         page_title='Stock Price Inspector AI Tool',
         page_icon='📈',
-        layout='centered'
+        # layout='centered'
     )
     st.title("OpenAI Stock Chatbot")
-    st.info("You can ask the bot about: price of a stock, % change over days, performance comparison of two or more stocks, and search about popular public listed companies.")
+    st.markdown("""
+                You can ask the bot about: 
+                price of a stock, trend of a stock over n days, performance comparison of two or more stocks, 
+                and search about latest news of a public listed company. Explicitly specify if you 
+                need a trend/line plot.
+                """)
     # plot_check = st.checkbox('Display plot')
-    # GPT Model
-    llm = ChatOpenAI(temperature=0, streaming=True, model="gpt-3.5-turbo-0613")
+    ## GPT Model
+    llm = OpenAI(temperature=0, streaming=True)
     # Tools for our agent to use
-    tools = [ddg_tool, StockPriceTool(), StockPctChangeTool(), StockGetBestPerformingTool()]
+    tools = [ddg_tool, StockPriceTool(), StockPctChangeTool()]
     
-    arg_tool = [StockPctChangeTool(), StockGetBestPerformingTool()]
-    functions = [format_tool_to_openai_function(t) for t in arg_tool]
+    # arg_tool = [StockPctChangeTool()]
+    # functions = [format_tool_to_openai_function(t) for t in arg_tool]
     
     open_ai_agent = initialize_agent(
         tools,
         llm,
-        agent=AgentType.OPENAI_FUNCTIONS,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        return_intermediate_steps=True,
         verbose=True
     )
     
@@ -205,26 +248,31 @@ if __name__=='__main__':
         st.chat_message("user").write(prompt)
         
         with st.chat_message("assistant"):
-            st.spinner('Generating ...')
-            st_callback = StreamlitCallbackHandler(st.container())
-            response = open_ai_agent.run(input=prompt, callbacks=[st_callback], return_intermediate_steps=True)
-            st.session_state.messages.append({'role':'assistant','content':response})
-            st.write(response)
-            # get the message arguments for plotting
-            ai_message = llm.predict_messages([HumanMessage(content=prompt)], functions=functions)
-            _args = json.loads(ai_message.additional_kwargs['function_call'].get('arguments'))
-            if _args.get('stocktickers'):
-                stock_codes = _args.get('stocktickers')
-            elif _args.get('stockticker'):
-                stock_codes = _args.get('stockticker')
-            else:
-                print('Stock code could not be found in the parameters used.')
-            days = _args.get('days_ago',0)
-            # print(ai_message)
-            if days > 0:
-                if isinstance(stock_codes, str):
-                    st.plotly_chart(get_hist_price_plot(stock_codes[0], days), use_container_width=True)
-                elif isinstance(stock_codes, list):
-                    st.plotly_chart(get_multiple_price_plot(stock_codes, days), use_container_width=True)
-                else:
-                    print(f"Cannot plot chart for {stock_codes}")
+            with st.spinner('Generating ...'):
+                st_callback = StreamlitCallbackHandler(st.container())
+                output = open_ai_agent(prompt)
+                response = output['output']
+                st.session_state.messages.append({'role':'assistant','content':response})
+                st.write(response)
+                # get the message arguments for plotting
+                steps_dict = output['intermediate_steps'][0][0].dict()
+                _args = steps_dict.get('tool_input')
+                code, days_ago = _args.split(',')
+                
+                if int(days_ago)>0:
+                    
+                    # if _args.get('stocktickers'):
+                    #     stock_codes = _args.get('stocktickers')
+                    # elif _args.get('stockticker'):
+                    #     stock_codes = _args.get('stockticker')
+                    # else:
+                    #     print('Stock code could not be found in the parameters used.')
+                        
+                    # days = _args.get('days_ago',0)
+                    # print(ai_message)
+                    # if isinstance(stock_codes, str):
+                    st.plotly_chart(get_hist_price_plot(code, int(days_ago)), use_container_width=True)
+                    # elif isinstance(stock_codes, list):
+                        # st.plotly_chart(get_multiple_price_plot(stock_codes, days), use_container_width=True)
+                    # else:
+                        # print(f"Cannot plot chart for {stock_codes}")
